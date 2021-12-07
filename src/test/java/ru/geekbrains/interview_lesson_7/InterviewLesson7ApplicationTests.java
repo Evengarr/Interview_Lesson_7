49b595b7170936ed61a553b921f7ddc7e40e9603
Evengarr <Ravasect@gmail.ru>
2021-12-07 22:57:58 +0300

    Initial commit

---
+++ src/test/java/ru/geekbrains/interview_lesson_7/InterviewLesson7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.interview_lesson_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewLesson7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
